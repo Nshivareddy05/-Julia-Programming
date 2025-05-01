73f6692cfd3a8020e58f4099647ce96561afcceb
N Shiva Manohara Reddy <164047244+Nshivareddy05@users.noreply.github.com>
2025-05-01 20:31:45 +0530

    Add files via upload

---
+++ input.jl
@@ -0,0 +1,8 @@
+#Taking input from the user
+println("Enter your name:")
+name = readline()
+
+println("Enter your age:")
+age = parse(Int, readline())  
+
+println("Hi $name, you are $age years old.")
